f412381e30396bcfeb793e6624ecc34484d21f2f
Cook Green <dell20091234@gmail.com>
2020-05-28 11:56:29 +0800

    Add SCO Writer

---
+++ TestWriter/frmMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SCO.TestWriter
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+			this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnMissionObjects = new System.Windows.Forms.Button();
+			this.btnGroundPaint = new System.Windows.Forms.Button();
+			this.btnAIMesh = new System.Windows.Forms.Button();
+			this.panelMain = new System.Windows.Forms.Panel();
+			this.menuStrip1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(991, 25);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuSave});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// mnuNew
+			// 
+			this.mnuNew.Name = "mnuNew";
+			this.mnuNew.Size = new System.Drawing.Size(180, 22);
+			this.mnuNew.Text = "New";
+			this.mnuNew.Click += new System.EventHandler(this.mnuNew_Click);
+			// 
+			// mnuSave
+			// 
+			this.mnuSave.Enabled = false;
+			this.mnuSave.Name = "mnuSave";
+			this.mnuSave.Size = new System.Drawing.Size(180, 22);
+			this.mnuSave.Text = "Save";
+			// 
+			// panel1
+			// 
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.btnAIMesh);
+			this.panel1.Controls.Add(this.btnGroundPaint);
+			this.panel1.Controls.Add(this.btnMissionObjects);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel1.Location = new System.Drawing.Point(0, 25);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 609);
+			this.panel1.TabIndex = 1;
+			// 
+			// btnMissionObjects
+			// 
+			this.btnMissionObjects.Enabled = false;
+			this.btnMissionObjects.Location = new System.Drawing.Point(33, 25);
+			this.btnMissionObjects.Name = "btnMissionObjects";
+			this.btnMissionObjects.Size = new System.Drawing.Size(121, 84);
+			this.btnMissionObjects.TabIndex = 0;
+			this.btnMissionObjects.Text = "Mission Objects";
+			this.btnMissionObjects.UseVisualStyleBackColor = true;
+			// 
+			// btnGroundPaint
+			// 
+			this.btnGroundPaint.Enabled = false;
+			this.btnGroundPaint.Location = new System.Drawing.Point(33, 154);
+			this.btnGroundPaint.Name = "btnGroundPaint";
+			this.btnGroundPaint.Size = new System.Drawing.Size(121, 84);
+			this.btnGroundPaint.TabIndex = 1;
+			this.btnGroundPaint.Text = "Groud Paint";
+			this.btnGroundPaint.UseVisualStyleBackColor = true;
+			// 
+			// btnAIMesh
+			// 
+			this.btnAIMesh.Enabled = false;
+			this.btnAIMesh.Location = new System.Drawing.Point(33, 287);
+			this.btnAIMesh.Name = "btnAIMesh";
+			this.btnAIMesh.Size = new System.Drawing.Size(121, 84);
+			this.btnAIMesh.TabIndex = 2;
+			this.btnAIMesh.Text = "AIMesh";
+			this.btnAIMesh.UseVisualStyleBackColor = true;
+			// 
+			// panelMain
+			// 
+			this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelMain.Location = new System.Drawing.Point(200, 25);
+			this.panelMain.Name = "panelMain";
+			this.panelMain.Size = new System.Drawing.Size(791, 609);
+			this.panelMain.TabIndex = 2;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(991, 634);
+			this.Controls.Add(this.panelMain);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.menuStrip1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "frmMain";
+			this.ShowIcon = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Mount&Blade SCO Writer";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem mnuNew;
+		private System.Windows.Forms.ToolStripMenuItem mnuSave;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button btnAIMesh;
+		private System.Windows.Forms.Button btnGroundPaint;
+		private System.Windows.Forms.Button btnMissionObjects;
+		private System.Windows.Forms.Panel panelMain;
+	}
+}
+
---
+++ TestWriter/frmMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCO.TestWriter
+{
+	public partial class frmMain : Form
+	{
+		private string currentFilePath = null;
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void mnuNew_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.Filter = "MB SceneObj File|*.sco";
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				currentFilePath = dialog.FileName;
+			}
+		}
+	}
+}
